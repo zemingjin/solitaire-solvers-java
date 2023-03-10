93f65b55d9511a9a55f408dd706b226f6d09079e
zemin <zeming.jin@gmail.com>
2023-03-10 06:45:50 -0600

    feature: added isSolved check in HSD search.

--- src/main/java/org/solitaire/model/SolveExecutor.java
+++ src/main/java/org/solitaire/model/SolveExecutor.java
@@ -13,19 +13,13 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-import static java.util.Comparator.comparingInt;
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
-import static java.util.stream.IntStream.range;
 import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 
 @SuppressWarnings("rawtypes")
-public class SolveExecutor<S, U, T extends Board<S, U>> implements GameSolver {
-    private final Function<List<T>, List<T>> checkReducingBoards = boards ->
-            isReducingBoards()
-                    ? range(boards.size() * 3 / 5, boards.size()).mapToObj(boards::get).toList()
-                    : boards;
+public class SolveExecutor<T extends Board<?, ?>> implements GameSolver {
     private static boolean singleSolution = false;
     private static int hsdDepth = 6;
 
@@ -33,14 +27,11 @@ public class SolveExecutor<S, U, T extends Board<S, U>> implements GameSolver {
     private final List<List> solutions = new ArrayList<>();
     private int totalScenarios = 0;
     private int maxStack = 0;
-    private boolean isPrint = true;
     private Function<T, T> cloner;
     private Consumer<T> solveBoard;
-    private boolean isReducingBoards = false;
 
     public SolveExecutor(T board) {
         addBoard(board);
-        solveBoard(singleSolution() ? this::solveByHSD : this::solveByDFS);
     }
 
     public SolveExecutor(T board, Function<T, T> cloner) {
@@ -56,13 +47,6 @@ public class SolveExecutor<S, U, T extends Board<S, U>> implements GameSolver {
         SolveExecutor.singleSolution = singleSolution;
     }
 
-    public boolean isPrint() {
-        return isPrint;
-    }
-
-    public void isPrint(boolean isPrint) {
-        this.isPrint = isPrint;
-    }
 
     public static int hsdDepth() {
         return hsdDepth;
@@ -78,14 +62,6 @@ public class SolveExecutor<S, U, T extends Board<S, U>> implements GameSolver {
 
     public void solveBoard(Consumer<T> solveBoard) {
         this.solveBoard = solveBoard;
-    }
-
-    public boolean isReducingBoards() {
-        return isReducingBoards;
-    }
-
-    public void isReducingBoards(boolean isReducingBoards) {
-        this.isReducingBoards = isReducingBoards;
     }
 
     @Override
@@ -106,46 +82,6 @@ public class SolveExecutor<S, U, T extends Board<S, U>> implements GameSolver {
         throw new RuntimeException(verify.toString());
     }
 
-    public void solveByDFS(T board) {
-        Optional.of(board.findCandidates())
-                .filter(ObjectUtils::isNotEmpty)
-                .map(it -> applyCandidates(it, board))
-                .map(Stream::toList)
-                .filter(ObjectUtils::isNotEmpty)
-                .map(checkReducingBoards)
-                .ifPresent(this::addBoards);
-    }
-
-    public void solveByHSD(T board) {
-        var boards = List.of(board);
-
-        for (int i = 1; i <= hsdDepth() && isNotEmpty(boards); i++) {
-            boards = boards.stream().flatMap(this::search).toList();
-        }
-        Optional.of(boards)
-                .filter(ObjectUtils::isNotEmpty)
-                .map(List::stream)
-                .map(it -> it.sorted(comparingInt(T::score)))
-                .map(this::getBestBoard)
-                .ifPresent(this::addBoard);
-    }
-
-    private Stream<T> search(T board) {
-        return Optional.of(board)
-                .map(T::findCandidates)
-                .filter(ObjectUtils::isNotEmpty)
-                .map(it -> applyCandidates(it, board))
-                .stream()
-                .flatMap(it -> it);
-    }
-
-    @SuppressWarnings("unchecked")
-    public Stream<T> applyCandidates(List<U> candidates, T board) {
-        return (Stream<T>) candidates.stream()
-                .map(it -> clone(board).updateBoard(it))
-                .filter(Objects::nonNull);
-    }
-
     public boolean isContinuing() {
         return !singleSolution || solutions.size() != 1;
     }
@@ -153,7 +89,7 @@ public class SolveExecutor<S, U, T extends Board<S, U>> implements GameSolver {
     public boolean checkBoard(T board) {
         if (board.isSolved()) {
             solutions.add(board.path());
-            if (nonNull(board.path()) && isNotEmpty(board.path()) && isPrint()) {
+            if (nonNull(board.path()) && isNotEmpty(board.path())) {
                 System.out.printf("%d: %s\n", board.path().size(), board.path());
             }
             return false;
